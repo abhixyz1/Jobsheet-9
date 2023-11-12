511aa81b662a98f95d0c9bfe1025fcf2832ec463
abhixyz1 <irfanhendra90@gmail.com>
2023-11-12 16:34:14 +0700

    percobaan linear search

--- LinearSearch20.java
+++ LinearSearch20.java
@@ -1,8 +1,9 @@
-public class LinearSearch20 {
-    
+import java.util.Scanner;
+public class LinearSearch20 { 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
+        /*int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
         int key = 20;
         int hasil = 0;
 
@@ -12,6 +13,25 @@ public class LinearSearch20 {
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);*/
+
+        System.out.print("Masukkan jumlah elemen array : ");
+        int array = sc.nextInt();
+
+        int [] array1 = new int[array];
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print("Masukkan elemen array ke-"+i+" : ");
+            array1[i] = sc.nextInt();  
+        }
+        System.out.print("Masukkan key yang ingin dicari :");
+        int keys = sc.nextInt();
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] == keys) {
+                System.out.println("Key ada diposisi indeks ke-" + i);
+            }
+
+        }
+            
+        
     }
 }
