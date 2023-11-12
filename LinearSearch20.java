e222e940d8697f493b2e9beb20d81b9741b1cc09
abhixyz1 <irfanhendra90@gmail.com>
2023-11-12 18:19:19 +0700

    percobaan ke 4 search

--- LinearSearch20.java
+++ LinearSearch20.java
@@ -3,7 +3,7 @@ public class LinearSearch20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        /*int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
+        /*int[] arrayInt = 9{34, 18, 26, 48, 72, 20, 56, 63};
         int key = 20;
         int hasil = 0;
 
@@ -15,23 +15,26 @@ public class LinearSearch20 {
         }
         System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);*/
 
+        int i;
+
         System.out.print("Masukkan jumlah elemen array : ");
         int array = sc.nextInt();
 
         int [] array1 = new int[array];
-        for (int i = 0; i < array1.length; i++) {
+        for (i = 0; i < array; i++) {
             System.out.print("Masukkan elemen array ke-"+i+" : ");
             array1[i] = sc.nextInt();  
         }
-        System.out.print("Masukkan key yang ingin dicari :");
+        System.out.print("Masukkan key yang ingin dicari : ");
         int keys = sc.nextInt();
-        for (int i = 0; i < array1.length; i++) {
+        for (i = 0; i < array; i++) {
             if (array1[i] == keys) {
                 System.out.println("Key ada diposisi indeks ke-" + i);
-            }
-
+                break;  
+            } 
         }
-            
-        
+        if (i == array) {
+            System.out.println("Key tidak ditemukan");    
+        }
     }
 }
